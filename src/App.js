29947efe8d7025229428c72187c74a7721874fcd
VP <vishay8@gmail.com>
2023-06-20 17:09:19 -0500

    fixed endpoint

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const App = () => {
       setIsLoading(true);
 
       // Make API request
-      const response = await axios.post("http://localhost:3001/search", {
+      const response = await axios.post("https://screener-api.onrender.com/search", {
         query,
       });
       //"https://screener-api.onrender.com/search"
